16ee6db568338b261873f8e334bea5c7edcc8647
joseph <josephchome888@gmail.com>
2025-03-09 18:20:12 +0900

    [Feat] User 엔티티

---
+++ src/main/java/com/example/url_shortener/user/User.java
@@ -0,0 +1,34 @@
+package com.example.url_shortener.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false)
+    private byte[] password; // VARBINARY(255) -> byte[]로 저장
+
+    @Column(nullable = false, length = 50)
+    private String role;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt = LocalDateTime.now();
+}
