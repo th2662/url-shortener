91771db347b88d63117837cf7b4be9226606e28f
joseph <josephchome888@gmail.com>
2025-04-06 19:28:39 +0900

    [Fix] Login 패키지 소문자 변경 -> login

--- src/main/java/com/example/url_shortener/login/LoginController.java
+++ src/main/java/com/example/url_shortener/login/LoginController.java
@@ -1,4 +1,4 @@
-package com.example.url_shortener.Login;
+package com.example.url_shortener.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
