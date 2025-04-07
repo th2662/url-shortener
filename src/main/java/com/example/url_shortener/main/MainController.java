cd967c8559368b9a280cfb972640471409ff8d1b
Chohanwool <josephchome888@gmail.com>
2025-04-08 02:09:19 +0900

    [init] Vue history 모드 관련 설정 변경

--- src/main/java/com/example/url_shortener/main/MainController.java
+++ src/main/java/com/example/url_shortener/main/MainController.java
@@ -1,13 +1,14 @@
 package com.example.url_shortener.main;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String mainPage() {
+    @RequestMapping(value = "/**/{path:[^\\.]+}")
+    public String redirect() {
         return "forward:/index.html";
     }
 }
+
