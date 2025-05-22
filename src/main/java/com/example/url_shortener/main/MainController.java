1e80779185d08f5ce62ff3b3f355e0785e6cf06e
joseph <josephchome888@gmail.com>
2025-05-22 21:49:03 +0900

    [security] MainController.java 전체 주석(불필요)

--- src/main/java/com/example/url_shortener/main/MainController.java
+++ src/main/java/com/example/url_shortener/main/MainController.java
@@ -1,14 +1,18 @@
-package com.example.url_shortener.main;
+/**
+ * vue + spring 에서는 WebConfig 만으로 충분하다고 한다.
+ */
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class MainController {
-
-    @RequestMapping(value = "/**/{path:[^\\.]+}")
-    public String redirect() {
-        return "forward:/index.html";
-    }
-}
-
+//package com.example.url_shortener.main;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class MainController {
+//
+//    @RequestMapping(value = "/**/{path:[^\\.]+}")
+//    public String redirect() {
+//        return "forward:/index.html";
+//    }
+//}
+//
